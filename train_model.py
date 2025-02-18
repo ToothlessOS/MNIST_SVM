3b9ef835b7b1300e687eed93e419cd04b56d103f
ToothlessOS <anthony.13661499167@gmail.com>
2025-02-18 12:52:40 +0800

    Change: Use RBF kernel

--- train_model.py
+++ train_model.py
@@ -6,7 +6,10 @@ import numpy as np
 train_data = get_images('Input_Data/train-images-idx3-ubyte/train-images.idx3-ubyte', length=50000)
 train_labels = get_labels('Input_Data/train-labels-idx1-ubyte/train-labels.idx1-ubyte')
 
-clf = svm.SVC()
+# Setup the kernel functions here
+clf = svm.SVC(kernel="rbf")
+print(f"INFO: Using {clf.kernel} kernels")
+print(f"INFO: Using hyperparameters of C={clf.C}, gamma={clf.gamma}")
 train_data = np.asarray(train_data[:(50000*784)]).reshape(50000, 784)
 
 clf.fit(train_data, train_labels[:50000])
